b531ff7af6eb739d2885acad5d769ca010767660
rogaciano <rogaciano@gmail.com>
2025-02-21 15:50:53 -0300

    Adicionado IP do servidor ao ALLOWED_HOSTS

--- banco/banco/settings.py
+++ banco/banco/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h=fvzi&b=entjtx_vgrwswj#q9uo4zp9=b7t9-1xe=hke$y$^l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '144.202.29.245']
 
 
 # Application definition
